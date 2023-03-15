b1848dcbce0883bc99bc18cfed7d373de90a38ce
RealR1M1 <nospam.nirimi@gmail.com>
2023-03-15 14:47:37 +0100

    Create Game.java

---
+++ src/main/java/com/github/hanyaeger/tutorial/Game.java
@@ -0,0 +1,23 @@
+package com.github.hanyaeger.tutorial;
+
+import com.github.hanyaeger.api.YaegerGame;
+
+public class Game extends YaegerGame {
+
+	@Override
+	public void setupGame() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setupScenes() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public static void main(String[] args){
+	    launch(args);
+	}
+
+}
